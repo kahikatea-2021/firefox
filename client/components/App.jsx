19c1679783da7b474b83497f430a1778e13bfa40
grace-malae <grace.t.malae@gmail.com>
2021-04-22 22:37:45 +1200

    install tailwindcss

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,35 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 
+import { fetchFruits } from '../actions'
+
 import Header from './Header'
 import Foxes from './Foxes'
 // import Fox from './Fox'
 import Footer from './Footer'
 
-import { fetchFruits } from '../actions'
+function App (props) {
+  useEffect(() => {
+    props.dispatch(fetchFruits())
+  }, [])
 
-function App(props) {
   return (
     <>
-      <Header />
-      <Foxes />
-      <Footer />
+      <div className='app'>
+        <h1>Fullstack Boilerplate - with Fruits!</h1>
+        <ul>
+          {props.fruits.map(fruit => (
+            <li key={fruit}>{fruit}</li>
+          ))}
+        </ul>
+      </div>
     </>
   )
+}
+const mapStateToProps = (globalState) => {
+  return {
+    fruits: globalState.fruits
+  }
 }
 
 export default connect(mapStateToProps)(App)
